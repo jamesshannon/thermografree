f46521bc2f9887a4970064745ca37d4c8013b81f
James Shannon <james@jamesshannon.com>
2019-01-27 00:43:25 -0800

    Added some helper functions
    To convert the array from dK to dC, and then to save as an image with opencv

--- src/htpa.py
+++ src/htpa.py
@@ -351,6 +351,34 @@ class HTPA:
   def __exit__(self, exc_type, exc_value, exc_traceback):
     self.close()
 
+def _to_celsius(dK, rnd=2):
+  """Convert degrees Kelvin to degrees Celsius.
+
+  Args:
+      vals (float,ndarray): K value (or array of values)
+      rnd (int, optional): Decimal points to round to
+
+  Returns:
+      float,ndarray: Rounded value(s) in degrees Celsius
+  """
+  return np.around(dK - 273.15, rnd)
+
+def _save_img(fname, celsius_arr):
+  img_range = (np.nanmin(celsius_arr), np.nanmax(celsius_arr))
+
+  # subtract the lower bound from the array so that that value becomes 0
+  #   then multiply everything by the multiplier to distribute the values
+  #   inside of the range
+  scale = 255 / (img_range[1] - img_range[0])
+
+  # astype() causes nan's to become 0
+  img = np.clip((celsius_arr - img_range[0]) * scale, 0, 255).astype('uint8')
+
+  img = cv2.applyColorMap(img, cv2.COLORMAP_SPRING)
+
+  cv2.imwrite(fname, img, options)
+
+
 if __name__ == '__main__':
   # FYI: The RPi already has 1.8k pull up resistors, so additional pullups are
   #   unnecessary and is likely to be out of I2C spec and will increase
