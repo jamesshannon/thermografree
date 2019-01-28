7151928b956901ec04fcc12a304c60d5fab5f263
James Shannon <james@jamesshannon.com>
2019-01-27 20:52:07 -0800

    Moved lookup table contents to .npz files
    Don’t need to keep contents in .py file, or load them into memory as part of the import. Happens to be a lot less disk usage, too.
    Also added all the metadata from the refs.h file, just in case it’s needed.

--- tests/test_lookup_tables.py
+++ tests/test_lookup_tables.py
@@ -3,11 +3,20 @@ import pytest
 
 from thermografree import lookup_tables
 
+def test_load_htpa_l2_1_datafile():
+  """Ensure that the HTPA datafile can be loaded and passes internal checks."""
+  ta_axes, dk_axes, table, offset = lookup_tables.get_table_and_axes()
+
+  assert table.shape == (1595, 12)
+
+
 def test_lookup_table_spreadsheet():
+  """Test the lookup table against the example lookup table spreadsheet."""
   v_vdd_comp = np.full((32,32), 182)
   a = lookup_tables.interpolate_tables(3000, v_vdd_comp,
                                        device='lookup_table_example')
   assert a[0][0] == pytest.approx(4026, rel=0.5)
+
 
 @pytest.mark.skip
 def test_lookup_table_datasheet():
