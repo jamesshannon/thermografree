7c0d00a677df6336b21b953b1e37fd53b4ad8019
James Shannon <james@jamesshannon.com>
2019-09-10 11:57:39 -0700

    Changes to capture_display() for testing

--- thermografree/capture_display.py
+++ thermografree/capture_display.py
@@ -1,6 +1,22 @@
+import time
+
+import numpy as np
+
 from thermografree.htpa import *
 
-with HTPA(0x1A, pull_ups=False) as dev:
-  im = dev.measure_temperatures()
+np.set_printoptions(threshold=1500, linewidth=150, precision=2)
 
-  print(to_celsius(im))
+print('* Creating HTPA device')
+with HTPA(0x1A, pull_ups=False) as dev:
+  while True:
+    print(time.ctime())
+    ambient = dev.measure_ambient_temperature()
+    print('Ambient Temp: {:+.1f}c'.format(to_celsius(ambient)))
+
+    im = dev.measure_temperatures()
+    print('32x32 image (celsius)')
+    print(to_celsius(im).astype('int16'))
+
+    time.sleep(10)
+
+    print('\n\n')
